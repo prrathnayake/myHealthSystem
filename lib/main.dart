101dace56df1bb5074828318b01beb1103dfead8
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2022-12-17 12:58:42 +0530

    set application orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:e_health/screens/welcome_screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
